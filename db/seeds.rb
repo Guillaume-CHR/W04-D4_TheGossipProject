a196b153fe2c653d2cbe9632b2662f3deaaa9d2f
guillaume-CHR <guillaume.christe@gmail.com>
2019-01-30 18:46:07 +0100

    Upload

---
+++ db/seeds.rb
@@ -0,0 +1,25 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
+require 'bundler'
+Bundler.require
+
+# 15.times do
+# 	User.create(name: Faker::Zelda.character)			
+# end
+
+# 7.times do
+# 	u = rand(1..15)
+# 	Pin.create(url: Faker::Internet.url, user_id: u)			
+# end
+
+# 10.times do
+# 	u = rand(1..15)
+# 	v = rand(1..7)
+# 	Comment.create(content: Faker::Hipster.paragraph, user_id: u, pin_id: v)			
+# end
