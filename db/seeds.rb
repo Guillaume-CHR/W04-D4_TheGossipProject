a985c1ea494b922c9dbd9dc3c24347d280f4356c
guillaume-CHR <guillaume.christe@gmail.com>
2019-01-31 18:26:13 +0100

    Final upload without readme so its last before last one

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,30 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'bundler'
+Bundler.require
+
+10.times do
+	City.create(name: Faker::Address.city, zip_code: Faker::Address.zip_code)			
+end
+
+60.times do
+	Patient.create(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, city_id: City.all.sample.id)			
+end
+
+20.times do
+	Doctor.create(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, city_id: City.all.sample.id)	
+end
+
+20.times do
+	Appointment.create(date: Faker::Date.between(365.days.ago, Date.today) ,doctor_id: Doctor.all.sample.id, patient_id: Patient.all.sample.id, city_id: City.all.sample.id)			
+end
+
+40.times do
+	Specialty.create(name: Faker::Educator.course)			
+end
+
+40.times do
+	JoinTableSpecialtyDoctor.create(specialty_id: Specialty.all.sample.id, doctor_id: Doctor.all.sample.id)			
+end
